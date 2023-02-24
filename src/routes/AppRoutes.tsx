7599de9d212af565de1aeaa2968c2725890616dc
Lucefull <luansteffens@gmail.com>
2023-02-24 11:57:57 -0300

    Style: adicionado iconas na navegação

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,30 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 import MelhoresProdutores from '../screens/BestProducers';
 import BestProducersRoutes from './BestProducersRoutes';
 import ProducerRoute from './ProducerRoute';
 
+import Coracao from '../assets/coracao.svg';
+import Home from '../assets/home.svg';
 const Tab = createBottomTabNavigator();
 
 const AppRoutes = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          headerShown: false,
+          tabBarActiveTintColor: '#2a9f85',
+          tabBarInactiveTintColor: '#c7c7c7',
+          tabBarIcon: ({color}) => {
+            let Icon = Home;
+            if (route.name === 'Melhores Produtores') {
+              Icon = Coracao;
+            }
+
+            return <Icon color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={ProducerRoute} />
         <Tab.Screen
           name="Melhores Produtores"
